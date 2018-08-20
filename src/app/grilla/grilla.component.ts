5889208ba32c2f332e3d4dbd4134561e30cc3c4d
pablo86v <pablo86v@gmail.com>
2018-08-20 20:19:19 +0100

    +grilla de personas

--- src/app/grilla/grilla.component.ts
+++ src/app/grilla/grilla.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
-import {PersonaService} from '../persona.service';
+import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
+import { PersonaService } from '../persona.service';
 
 
 @Component({
@@ -10,27 +10,88 @@ import {PersonaService} from '../persona.service';
 })
 export class GrillaComponent implements OnInit {
 
-  constructor() { }
+  private source: LocalDataSource;
+  private personas: Array<any> = [];
+  public listaPersonas;
+
+
+  constructor(public servicio: PersonaService) {
+    this.source = new LocalDataSource();
+    this.getDatosSmartTable();
+    this.getDatosGrillaDiv();
+
+  }//constructor 
+
+
+  //************************************************/
+  //        MIS FUNCIONES
+  //************************************************/
+
+  getDatosSmartTable() {
+    this.servicio.getData()
+      .subscribe(
+      data => this.personas = data,
+      error => console.error("Error: " + error),
+      () => this.source.load(this.personas)
+      );
+  }
+
+  getDatosGrillaDiv() {
+
+    this.servicio.getData()
+      .subscribe(
+      data => this.listaPersonas = data,
+      error => console.error("Error: " + error),
+      () => this.source.load(this.personas)
+      );
+  }
+
+
+  getColorBySex(sexo): string {
+    if (sexo == "F") {
+      return "#FE2EF7";
+    } else {
+      return "#0000FF";
+    }
+  }
+
+  eliminarPersona(id) {
+    this.servicio.deletePersona(id)
+      .subscribe(
+      data => console.log(data),
+      error => console.error("Error: " + error),
+      () => this.getDatosGrillaDiv()
+      );
+  }
+
+
 
   ngOnInit() {
   }
 
-settings = {
-  columns: {
-    id: {
-      title: 'ID'
-    },
-    name: {
-      title: 'Full Name'
-    },
-    username: {
-      title: 'User Name'
-    },
-    email: {
-      title: 'Email'
-    }
-  }
-};
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      nombre: {
+        title: 'NOMBRE'
+      },
+      apellido: {
+        title: 'APELLIDO'
+      },
+      dni: {
+        title: 'DNI'
+      },
+      foto: {
+        title: 'FOTO'
+      },
+      sexo: {
+        title: 'SEXO'
+      }
+
+    } //columns
+  }; //settings
 
 
 
--- src/app/persona.service.ts
+++ src/app/persona.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+
 
 @Injectable()
 export class PersonaService {
 
-  constructor() { }
+  constructor(public http: Http) { }
 
 
 
-}
+  getData() {
+    return this.http.get('http://pablovalenzuela.esy.es/index.php/ge?t=persona')
+      .map(response => response.json());
+  }
+
+
+  deletePersona(id) {
+    return this.http.delete('http://pablovalenzuela.esy.es/index.php/de/' + id + '?t=persona')
+      .map(response => response.json());
+  }
+
+
+}//class
+
+
